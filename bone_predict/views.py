b25410bab123e7a06385fe318f643114135c8cf6
SamimSn <samim83sanaee@gmail.com>
2024-10-04 16:02:35 +0330

    fix: save

--- bone_predict/views.py
+++ bone_predict/views.py
@@ -1,5 +1,4 @@
 from rest_framework.viewsets import ModelViewSet
-import rest_framework.permissions as permissions
 from .models import BoneImage
 from .serializers import BoneImageSerializer
 from .utils import predict
@@ -7,8 +6,7 @@ from .utils import predict
 
 class BoneImageViewset(ModelViewSet):
     queryset = BoneImage.objects.all()
-    serializer_class = BoneImageSerializer
-    permission_classes = [permissions.IsAdminUser]
+    serializer_class = BoneImageSerializer    
     
     def perform_create(self, serializer):
             bone_image: BoneImage = serializer.save()
